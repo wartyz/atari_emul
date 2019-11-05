8b77b49d8b158e5be47fddf96780d6ea2bed0e02
wartyz <zwenderk@hotmail.com>
2019-11-05 20:02:20 +0100

    primer commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+// https://www.youtube.com/watch?v=pcqKVQ_bK9A&list=PLJ0INvSnPQjPqw4SiiSVMCDAb3dllSs_M&index=1
+// 3:42:04
+#![allow(non_snake_case)]
+
+use main::cpu::*;
+use main::atari::Atari;
+use main::debugger::Debugger;
+use main::tests::*;
+
+fn main() {
+    let mut atari = Atari::new();
+
+
+    //Tests::CPUFlagsTest(&mut atari.mCPU);
+    //let mut debugger = Debugger::DumpCPU(&atari.mCPU);
+    //Tests::ADC_iTest(&mut atari.mCPU);
+    Tests::AddrTest(&mut atari.mCPU);
+    //Tests::OPTest(&mut atari.mCPU);
+
+    println!("Done!");
+}
+
+#[cfg(test)]
+mod tests {
+    use main::atari::Atari;
+
+    #[test]
+    fn prueba() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
+
